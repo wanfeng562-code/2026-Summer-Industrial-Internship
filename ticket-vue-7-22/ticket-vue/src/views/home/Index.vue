202a32e0457e40f8cad325524e5d1c69916a7081
Breeze <2865167171@qq.com>
2026-07-22 20:26:56 +0800

    feat(frontend): add Vue ticket management interface

---
+++ ticket-vue-7-22/ticket-vue/src/views/home/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div>
+  <h1>系统登录之后的首页</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ ticket-vue-7-22/ticket-vue/src/views/login/RegisterView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div>
+  <h1>注册页面，大家课后练习，完成项目的注册功能</h1>
+  </div>
+</template>
+
+<style scoped></style>
