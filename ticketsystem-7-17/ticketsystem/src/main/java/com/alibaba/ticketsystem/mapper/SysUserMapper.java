cc9230770764c9981cba2c2823f6a37a72872a00
Breeze <2865167171@qq.com>
2026-07-27 14:46:13 +0800

    refactor(backend): promote active backend and remove dated snapshots

--- ticketsystem-7-17/ticketsystem/src/main/java/com/alibaba/ticketsystem/mapper/SysUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.ticketsystem.mapper;
-
-import com.alibaba.ticketsystem.entity.SysUser;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface SysUserMapper extends BaseMapper<SysUser> {
-}
